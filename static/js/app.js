87fdd573929c8e12e5d982335e50260b9aa9ab30
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-04-06 22:37:13 -0700

    checkpoint save

--- static/js/app.js
+++ static/js/app.js
@@ -1,69 +1,75 @@
 // Create function for plot graphs
-// function init(sample) {
+// function plotsData(sample) {
 
     // Use the D3 library to read in samples.json.
-    let dataSet = d3.json('data/samples.json').then(data => {
+    d3.json('data/samples.json').then(data => {
         console.log(data);
+        
+        
+        // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+        // Use sample_values as the values for the bar chart.
+        // Use otu_ids as the labels for the bar chart.
+        // Use otu_labels as the hovertext for the chart.
+
+        // Object.entries(sample).forEach(function ([key, value]))
+
+        let values = data.samples.sample_values;
+        // let labels = data.samples.otu_ids;
+        // let hovertext = data.samples.otu_labels;
+        console.log(values);
+        // console.log(labels);
+        // console.log(hovertext);
+
     });
 
-    // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-    // Use sample_values as the values for the bar chart.
-    // Use otu_ids as the labels for the bar chart.
-    // Use otu_labels as the hovertext for the chart.
+        //     const trace1 = {
+        //         x: values,
+        //         y: labels,
+        //         text: hovertext,
+        //         type: 'bar',
+        //         orientation: 'h'
+        //     };
 
-//     Object.entries(sample).forEach(function ([key, value]) {
-        
-//     })
+        //     const barData = [trace1];
 
-    let values = dataSet.samples.sample_values.slice(0,10);
-    let labels = dataSet.samples.otu_ids.slice(0,10);
-    let hovertext = dataSet.samples.otu_labels.slice(0,10);
+        //     const layout = {
+        //         title: "Top 10 OTUs found",
+        //         // xaxis:
+        //         // yaxis:
+        //     };
 
-//     const trace1 = {
-//         x: values,
-//         y: labels,
-//         text: hovertext,
-//         type: 'bar',
-//         orientation: 'h'
-//     };
+        //     // plot out bar chat
+        //     Plotly.newPlot('bar', barData, layout);
 
-//     const barData = [trace1];
-
-//     const layout = {
-//         title: "Top 10 OTUs found",
-//         // xaxis:
-//         // yaxis:
-//     };
-
-//     // plot out bar chat
-//     Plotly.newPlot('bar', barData, layout);
-
-
-
-//     });
 
 // };
 
 
-    // Create a bubble chart that displays each sample.
-    // Use otu_ids for the x values.
-    // Use sample_values for the y values.
-    // Use sample_values for the marker size.
-    // Use otu_ids for the marker colors.
-    // Use otu_labels for the text values.
+        // Create a bubble chart that displays each sample.
+        // Use otu_ids for the x values.
+        // Use sample_values for the y values.
+        // Use sample_values for the marker size.
+        // Use otu_ids for the marker colors.
+        // Use otu_labels for the text values.
+
+        // const trace2 = {
+        //     x: ,
+        //     y: ,
+        //     markerSize: ,
+        //     markerColor: ,
+        //     text:
+        // };
+
+        // const bubbleData = [trace2];
+
+        // const layout2 = {
+        //     xaxis: { title: "Bubble Title" }        
+        // };
+
+        // Plotly.newPlot('bubble', bubbleData, layout2);
 
 
-
-
-
-
-    // Display the sample metadata, i.e., an individual's demographic information.
-
-
-
-
-
-
+        // Display the sample metadata, i.e., an individual's demographic information.
 
 
 
@@ -77,13 +83,22 @@
 
 
     // Update all of the plots any time that a new sample is selected.
+
+    // // Call updatePlotly function when a change takes place to the DOM
+    // d3.select('#selDataset').on('change', updatePlotly)
+
     // function updatePlotly() {
-    //     console.log('Testing button')
-    // }
-    // d3.select('#selDataset').on('change', updatePlotly);
 
+    //     let barChart = d3.event.target.value;
+    //     console.log(barChart);
 
-
+    //     if (barChart === 'default') {
+    //         Plotly.restyle('bar', 'x', [defaultData]);
+    //         Plotly.restyle('bar', 'y', [defaultData]);
+    //     } else {
+    //         Plotly.restyle('bar', 'x', [barCharts.filter(set => set.names === barChart).x]);
+    //         Plotly.restyle('bar', 'y', [barCharts.filter(set => set.names === barChart).y]);
+    //     }
 
 
 
