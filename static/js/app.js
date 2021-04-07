34dc58aef5b72bb5c6b4c2deec1267c6448cbc4c
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-04-06 20:19:55 -0700

    checkpoint save

--- static/js/app.js
+++ static/js/app.js
@@ -1,71 +1,63 @@
 // Create function for plot graphs
-function init(sample) {
+// function init(sample) {
 
-// Use the D3 library to read in samples.json.
-    d3.json('data/samples.json').then(data => {
+    // Use the D3 library to read in samples.json.
+    let dataSet = d3.json('data/samples.json').then(data => {
         console.log(data);
-
+    });
 
     // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
     // Use sample_values as the values for the bar chart.
     // Use otu_ids as the labels for the bar chart.
     // Use otu_labels as the hovertext for the chart.
 
-    Object.entries(sample).forEach(function ([key, value]) {
+//     Object.entries(sample).forEach(function ([key, value]) {
         
-    })
+//     })
 
-    let values = data.sample_values.slice(0,10);
-    let labels = data.otu_ids.slice(0,10);
-    let hovertext = data.otu_labels.slice(0,10);
+    let values = dataSet.samples.sample_values.slice(0,10);
+    let labels = dataSet.samples.otu_ids.slice(0,10);
+    let hovertext = dataSet.samples.otu_labels.slice(0,10);
 
-    const trace1 = {
-        x: values,
-        y: labels,
-        text: hovertext,
-        type: 'bar',
-        orientation: 'h'
-    };
+//     const trace1 = {
+//         x: values,
+//         y: labels,
+//         text: hovertext,
+//         type: 'bar',
+//         orientation: 'h'
+//     };
 
-    const barData = [trace1];
+//     const barData = [trace1];
 
-    const layout = {
-        title: "Top 10 OTUs found",
-        // xaxis:
-        // yaxis:
-    };
+//     const layout = {
+//         title: "Top 10 OTUs found",
+//         // xaxis:
+//         // yaxis:
+//     };
 
-    // plot out bar chat
-    Plotly.newPlot('bar', barData, layout);
-
-
-
-});
-
-// Create a bubble chart that displays each sample.
-// Use otu_ids for the x values.
-// Use sample_values for the y values.
-// Use sample_values for the marker size.
-// Use otu_ids for the marker colors.
-// Use otu_labels for the text values.
+//     // plot out bar chat
+//     Plotly.newPlot('bar', barData, layout);
 
 
 
+//     });
+
+// };
 
 
-
-// Display the sample metadata, i.e., an individual's demographic information.
+    // Create a bubble chart that displays each sample.
+    // Use otu_ids for the x values.
+    // Use sample_values for the y values.
+    // Use sample_values for the marker size.
+    // Use otu_ids for the marker colors.
+    // Use otu_labels for the text values.
 
 
 
 
 
 
-
-
-
-
-// Display each key-value pair from the metadata JSON object somewhere on the page.
+    // Display the sample metadata, i.e., an individual's demographic information.
 
 
 
@@ -73,8 +65,26 @@ function init(sample) {
 
 
 
-// Update all of the plots any time that a new sample is selected.
 
 
-};
-init();
+
+    // Display each key-value pair from the metadata JSON object somewhere on the page.
+
+
+
+
+
+
+
+    // Update all of the plots any time that a new sample is selected.
+    // function updatePlotly() {
+    //     console.log('Testing button')
+    // }
+    // d3.select('#selDataset').on('change', updatePlotly);
+
+
+
+
+
+
+    // init();
